d48ec7840c7bed62351e92fa2c09f792b7729af7
Rino <rino.schu@gmail.com>
2024-03-31 16:40:19 +0200

    Continued day3

--- 2023/Day3/Part1.py
+++ 2023/Day3/Part1.py
@@ -6,50 +6,49 @@ def get_numbers(s):
     return array
 
 
-def check_line_below(val, idx):
-    for i in range(10 + idx - 1, 10 + idx + len(val) + 1):
-        if content[i] == '.':
-            return False
-    return True
+def line_below_is_hit(val, idx):
+    for i in range(10 + idx - 1, 10 + idx + len(val)):
+        if content[i] not in ". ":
+            return True
+    return False
 
 
-def check_line_above(val, idx):
-    for i in range(-10 + idx - 1, -10 + idx + len(val) + 1):
-        if content[i] == '.':
-            return False
-    return True
+def line_above_is_hit(val, idx):
+    for i in range(-10 + idx - 1, -10 + idx + len(val)):
+        if content[i] not in ". ":
+            return True
+    return False
 
 
 file = open("Input.txt", "r")
-
 content = file.read()
 
 sum = 0
-count = 0
 
 for num in get_numbers(content):
     value = str(num)
     idx = content.strip().replace("\n", "").find(value)
-    print(num + ": Index is {index}".format(index=idx))
+    # print(num + ": Index is {index}".format(index=idx))
+
+    # first check if before or after word in current line is anything except a point
     if content[idx - 1] != '.' and content[idx + len(value)] != '.':
-        print("Current line was already enough with number {number} and Index before: {idx1} and index after: {idx2}"
+        print("Current line hit for {number}. Index before: {idx1} and index after: {idx2}"
               .format(number=num, idx1=content[idx-1], idx2=content[idx + len(value)]))
         sum += int(num)
-        count += 1
     else:
-        count += 1
-        if count == 1:
-            if check_line_below(value, idx):
-                print(num + ": First line is valid")
+        if idx < 10:  # for all nums in first line, only check line below
+            if line_below_is_hit(value, idx):
+                print(num + ": Line below hits number")
                 sum += int(num)
-        elif count == len(file.readlines()):
-            if check_line_above(value, idx):
-                print(num + ": Last line is valid")
+        # for all nums in last line, only check lines above
+        elif idx >= 10 * len(file.readlines()) - 1:
+            if line_above_is_hit(value, idx):
+                print(num + ": Line above hits number")
                 sum += int(num)
         else:
-            if check_line_above(value, idx) and check_line_below(value, idx):
-                print(num + ": InBetween line is valid")
+            if line_above_is_hit(value, idx) or line_below_is_hit(value, idx):
+                print(num + ": Line above or below is a hit")
                 sum += int(num)
 
-
+file.close()
 print(sum)
